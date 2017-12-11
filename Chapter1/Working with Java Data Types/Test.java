b76071f0b77207d0fe111610f1cd5374a21d3444
Hugo Alejandro <hag8194@gmail.com>
2017-12-10 21:03:19 -0400

    Updated some files

--- Chapter1/Working with Java Data Types/Test.java
+++ Chapter1/Working with Java Data Types/Test.java
@@ -34,4 +34,17 @@ public class Test
 		System.out.println(new Test().testVar); //Fields in a class are automatically initialized
 		System.out.println($123testo_var4);
 	}
+
+	public void findAnswer(boolean check) {
+		int answer;
+		int onlyOneBranch;
+		if (check) {
+		onlyOneBranch = 1;
+		answer = 1;
+		} else {
+		answer = 2;
+		}
+		System.out.println(answer);
+		System.out.println(onlyOneBranch); // DOES NOT COMPILE
+	}
 }
